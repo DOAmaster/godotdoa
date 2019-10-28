980e710a4d67749fd88c46ee7cc5842919f58bfa
Student <student@lab-311-018.cs.csubak.edu>
2019-10-28 12:49:04 -0700

    added rng tiles

--- sayhello.gd
+++ sayhello.gd
@@ -5,6 +5,9 @@ extends Panel
 # var b = "text"
 const MySmokeResource = preload("res://player/Player.tscn")
 
+var eventTotal = 100
+var eventCount = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,9 +25,21 @@ func _on_Button_pressed():
 	#You could now make changes to the new instance if you wanted
 	#CurrentEntry.name = "SmokeA"
 	#Attach it to the tree
+	
+	#get parent node and spawn Player on left side of screen
 	var myPar = self.get_parent()
-	GrabedInstance.position = Vector2(100,0)
+	GrabedInstance.position = Vector2(100,1000)
 	myPar.add_child(GrabedInstance)
+	
+	#generate random events on map
+	#Get games tile map from parent node
+	var myTileMap = myPar.get_node("TileMap");
+	#myTileMap.set_cell(20, 16, 0, false, false, false, Vector2(0,0))
+	myTileMap.set_cell(7, 16, 23)
+	
+	#remove panel from tree
 	queue_free()
+	
+	
 	#self.add_child(GrabedInstance)
 	pass # Replace with function body.
